8a0f861262b9c6426741a1866bd45a76063bc780
daw4332 <4166712@alu.murciaeduca.es>
2026-04-06 11:13:31 +0200

    Primmer commit

---
+++ main.js
@@ -0,0 +1,39 @@
+const header = document.querySelector('header');
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links a');
+
+// Función para scroll con offset
+function scrollToSection(target) {
+    const headerOffset = header.offsetHeight;
+    const elementPosition = target.getBoundingClientRect().top + window.pageYOffset;
+    const offsetPosition = elementPosition - headerOffset;
+
+    window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+    });
+}
+
+// Scroll suave al hacer click en cualquier enlace
+navLinks.forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        const href = link.getAttribute('href');
+
+        if (href === "#") {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        } else {
+            const target = document.querySelector(href);
+            scrollToSection(target);
+        }
+
+        // Cierra el menú si está activo (móviles)
+        navMenu.classList.remove('active');
+    });
+});
+
+// Abrir/cerrar menú hamburguesa
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
